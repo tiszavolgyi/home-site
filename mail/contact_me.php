6442c50e6b44754d5ac602288cfb528baedfa3f5
Peter Tiszavolgyi <tiszavolgyi@gmail.com>
2018-10-16 14:02:48 -0700

    message to file

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -8,17 +8,25 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-	
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
+
+$file = 'messages.txt';
+// Open the file to get existing content
+$current = file_get_contents($file);
+// Append a new person to the file
+$current .= $name,"\n", $email_address, "\n", $message;
+// Write the contents back to the file
+file_put_contents($file, $current);
+
 // Create the email and send the message
 $to = 'tiszavolgyi@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: tiszavolgyi@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
 ?>
